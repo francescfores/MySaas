782795a690cb1141262198d688b278579f90bb8e
Francesc Fores <francescfores@iesebre.com>
2016-04-04 20:47:31 +0200

    Convert Html to PDF

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -2,12 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use DOMPDF;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Http\Response;
+use View;
 
 class PDFController extends Controller
-{ public function downloadInvoice(){
-    echo "eeee";
-}
+    {
+
+    public function invoiceHtml(){
+        $data = [
+            'vendor' => "PROVA",
+            'user' => "fran"
+        ];
+
+    return view('receipt', $data);
+    }
+
+    public function downloadInvoice(){
+
+        if (! defined('DOMPDF_ENABLE_AUTOLOAD')) {
+            define('DOMPDF_ENABLE_AUTOLOAD', false);
+        }
+        if (file_exists($configPath = base_path().'/vendor/dompdf/dompdf/dompdf_config.inc.php')) {
+            require_once $configPath;
+        }
+
+        $dompdf = new DOMPDF;
+        //$dompdf->load_html("<h1>Hei</h1>"); //pasar-li la plantilla
+        $data = [
+            'vendor' => "PROVA",
+            'user' => [
+                "email" =>" email"
+            ]
+        ];
+        $dompdf->load_html($this->view($data)->render()); //data
+        $dompdf->render();
+
+        return $this->download($dompdf->output());
+
+       //return view('invoice');
+    }
+
+
+    public function download($pdf){
+
+      //$filename = 'prova_'.$this->date()->month.'_'.$this->date()->year.'.pdf';
+        $filename =  "hola.pdf";
+
+        return new Response($pdf, 200, [
+            'Content-Description' => 'File Transfer',
+            //'Content-Disposition' => 'attachment; filename="'.$filename.'"',
+            'Content-Transfer-Encoding' => 'binary',
+            'Content-Type' => 'application/pdf',
+        ]);
+    }
+
+    public function view( $data)
+    {
+
+        return View::make('receipt',$data);
+    }
+
+
 }
